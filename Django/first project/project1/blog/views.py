32cba6b0e97bec78858f2b94b74b6d2f7c571471
aliPMPAINT <ali.PMPAINT@tutanota.com>
2019-12-05 21:06:39 +0330

    more

--- Django/first project/project1/blog/views.py
+++ Django/first project/project1/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 comments = [
 
@@ -32,7 +33,7 @@ comments = [
 ]
 
 def home(request):
-    context = { "comments": comments}
+    context = { "comments": Post.objects.all()}
 
     return render(request, "blog/home.html", context)
 
